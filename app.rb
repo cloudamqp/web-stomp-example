803f02172e9f2767c8181b75204294b5c0bb6344
Anton Dalgren <anton@84codes.com>
2019-05-06 22:19:38 +0200

    Update code to work with rabbitmq 3.7

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   get '/' do
     u = URI.parse ENV['CLOUDAMQP_URL'] || 'amqp://guest:guest@localhost'
     @ws_stomp_uri = {
-      protocol: u.scheme.sub('amqp', 'http'),
+      protocol: u.scheme.sub('amqp', 'ws'),
       host: u.host,
       vhost: u.path[1..-1] || '/',
       user: "#{u.user}-web",
